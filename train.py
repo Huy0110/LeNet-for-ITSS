cc5a4f728d17a697736cf75f0513480e3bbdf63c
Huy0110 <duchuy01102001@gmail.com>
2022-12-08 18:28:29 +0000

    add tensorboard to visuallize trainning and add validation

--- train.py
+++ train.py
@@ -8,6 +8,8 @@ from torch import optim
 import torch.nn as nn
 import torch.nn.functional as F
 import time
+from torch.utils.tensorboard import SummaryWriter
+writer = SummaryWriter()
 from model import Lenet
 from dataloader_mnist import trainloader, testloader, trainset, testset
 
@@ -20,6 +22,28 @@ lenet.cuda()
 
 criterian = nn.CrossEntropyLoss(size_average=False)
 optimizer = optim.SGD(lenet.parameters(), lr=learning_rate)
+
+
+def validation(i):
+    testloss = 0.
+    testacc = 0.
+    lenet.eval()
+    for (img, label) in testloader:
+        img = Variable(img).cuda()
+        label = Variable(label).cuda()
+        
+        output = lenet(img)
+        loss = criterian(output, label)
+        testloss += loss.item()
+        _, predict = torch.max(output, 1)
+        num_correct = (predict == label).sum()
+        testacc += num_correct.item()
+
+    testloss /= len(testset)
+    testacc /= len(testset)
+    writer.add_scalar('Validation/loss', testloss, i+1)
+    writer.add_scalar('Validation/acc', 100*testacc, i+1)
+    lenet.train()
 
 # train
 for i in range(epoches):
@@ -49,3 +73,8 @@ for i in range(epoches):
     running_loss /= len(trainset)
     running_acc /= len(trainset)
     print("[%d/%d] Loss: %.5f, Acc: %.2f, Time: %.1f s" %(i+1, epoches, running_loss, 100*running_acc, time.time()-since))
+    validation(i)
+    writer.add_scalar('Trainning/loss', running_loss, i+1)
+    writer.add_scalar('Trainning/acc', 100*running_acc, i+1)
+
+
